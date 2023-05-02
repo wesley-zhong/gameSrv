2642b945010c267ca9c58dacf461b7cb29d6bedb
wesley <wiqi.zhong@gmail.com>
2023-05-02 22:16:22 +0800

    add  stop all

--- cmd/stopAll.sh
+++ cmd/stopAll.sh
@@ -1,3 +1,7 @@
 ps -ef|grep gateway|grep -v grep|awk '{print $2}' | xargs kill -9
 ps -ef|grep game|grep -v grep|awk '{print $2}' | xargs kill -9
 ps -ef|grep world|grep -v grep|awk '{print $2}' | xargs kill -9
+
+ps -aux | grep game
+ps -aux | grep gateway
+ps -aux | grep world
