baa5d6ecb1c8a57b28facae8e0f253fa7ed36ac5
wesley <wiqi.zhong@gmail.com>
2023-11-12 19:14:34 +0800

    for compiler

--- pkg/quadtree/quadtree_test.go
+++ pkg/quadtree/quadtree_test.go
@@ -1,6 +1,11 @@
 package quadtree
 
-import "testing"
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
 
 type MapUnit struct {
 	Uid int64
@@ -17,16 +22,77 @@ func Test_add_test(t *testing.T) {
 		Nodes:      nil,
 		Total:      0,
 	}
-	mapUnit := &MapUnit{
-		Uid:    0,
-		Bounds: Bounds{100, 100, 20, 30},
+	startTime := time.Now().UnixMilli()
+	for i := 0; i < 300000; i++ {
+		x := rand.Int31n(307200)
+		y := rand.Int31n(307200)
+		//fmt.Printf("   pppppp x = %d y =%d \n", x, y)
+		mapUnit := &MapUnit{
+			Uid:    0,
+			Bounds: Bounds{float64(x), float64(y), 256, 256},
+		}
+		entity := Entity[MapUnit]{
+			obj:    mapUnit,
+			Bounds: mapUnit.Bounds,
+		}
+		qt.Insert(entity)
 	}
-	entity := Entity[MapUnit]{
-		obj:    mapUnit,
-		Bounds: mapUnit.Bounds,
+	endTime := time.Now().UnixMilli()
+	fmt.Printf("--------  cost time =%d \n", endTime-startTime)
+
+	sStart := time.Now().UnixMilli()
+	count := 0
+	for i := 0; i < 10000; i++ {
+		retrieve := qt.Retrieve(Bounds{
+			X:      600,
+			Y:      700,
+			Width:  2000,
+			Height: 2000,
+		})
+		count = len(retrieve)
 	}
-	qt.Insert(entity)
-	if qt.Total != 1 {
-		t.Errorf("expect %d but =%d", 1, qt.Total)
-	}
+	sEnd := time.Now().UnixMilli()
+
+	fmt.Printf("-----------cost  = %d   count = %d\n", sEnd-sStart, count)
+}
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func Test_Reference(t *testing.T) {
+	// 创建一个Person结构体实例
+	p := Person{Name: "张三", Age: 18}
+	// 创建一个指向Person结构体实例的指针
+	p1 := &p
+
+	// 输出Person结构体实例的信息
+	fmt.Println(p.Name, p.Age)
+
+	// 通过指针修改Person结构体实例的信息
+	p1.Name = "李四"
+	p1.Age = 20
+
+	fmt.Println(p.Name, p.Age)
+
+	p2 := p
+	p2.Name = "王五"
+	p2.Age = 22
+	acceptValue(*p1)
+
+	// 再次输出Person结构体实例的信息
+	fmt.Println(p.Name, p.Age)
+
+	acceptPoint(p1)
+
+	fmt.Println(p.Name, p.Age)
+}
+
+func acceptValue(person Person) {
+	person.Name = "hello"
+}
+
+func acceptPoint(person *Person) {
+	person.Name = "hello1"
 }
