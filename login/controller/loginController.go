fd6c83ddf1918e3155aa89746de76b0408f59afe
wesley <wiqi.zhong@gmail.com>
2023-12-11 10:50:23 +0800

    code optimize

--- login/controller/loginController.go
+++ login/controller/loginController.go
@@ -1,5 +1,10 @@
 package controller
 
+import (
+	"gameSrv/login/service"
+	"gameSrv/pkg/log"
+)
+
 type Login struct {
 }
 
@@ -13,6 +18,11 @@ type LoginRes struct {
 }
 
 func (login *Login) Login(loginDto *LoginReq) *LoginRes {
+	account := service.FindPlayerAccount(loginDto.AccountId)
+	if account == nil {
+		log.Warnf(" countId = %s not found", loginDto.AccountId)
+		return nil
+	}
 	return &LoginRes{
 		Pid: 11111,
 	}
